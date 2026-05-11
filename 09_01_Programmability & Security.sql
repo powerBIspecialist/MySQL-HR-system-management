601b925201e5feb4721cf59b296a64872d1ebcc0
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-11 15:19:07 +0300

    Create 09_01_Programmability & Security.sql

---
+++ 09_01_Programmability & Security.sql
@@ -0,0 +1,7 @@
+-- Stored procedures (IN/OUT parameters)
+-- Trigger BEFORE / AFTER
+-- Differences: FUNCTION VS PROCEDURE
+-- CREATE USER, GRANT, REVOKE
+-- Export/ import mysqldump, sql files
+
+-- --THE END--
