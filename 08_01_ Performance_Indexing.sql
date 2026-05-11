53f95ac81afa184f705f79611e460ebeb89e8028
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-11 15:16:07 +0300

    Create 08_01_ Performance_Indexing.sql

---
+++ 08_01_ Performance_Indexing.sql
@@ -0,0 +1,7 @@
+-- Create INDEX
+-- Explain
+-- Indexes on WHERE and JOIN columns
+-- Composite indexes (multi-column)
+-- Avoiding full table scans, query optimization
+
+-- -- PRoject: Explain analysis + relevant indexes
