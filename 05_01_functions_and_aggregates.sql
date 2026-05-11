384b3a58014205620eefa82bbf8b47e25944a00a
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-11 17:03:24 +0300

    Update 05_01_functions_and_aggregates.sql

--- 05_01_functions_and_aggregates.sql
+++ 05_01_functions_and_aggregates.sql
@@ -9,45 +9,45 @@
 -- NULL functions: IFNULL, NULLIF
 
 
-  --COUNT – Total Employees
+  -- 01. COUNT – Total Employees
   -- Counts the total number of employees in the company
 SELECT COUNT(*) AS total_employees
   FROM employees;
 
 
-  -- COUNT + GROUP BY – Employees per Department
-  -- Shows how many employees exist in each department.
+  -- 02. COUNT + GROUP BY – Employees per Department
+  -- Shows how many employees exist in each department
 SELECT 
     dept_id,
     COUNT(*) AS employee_count
 FROM employees
 GROUP BY dept_id;
 
-  -- SUM – Total Salary Cost
+  -- 03. SUM – Total Salary Cost
   -- Calculates total payroll cost of all employees
 SELECT 
     SUM(salary) AS total_salary_budget
 FROM employees;
 
-  -- AVG – Average Salary
-  -- Returns the average salary in the company.
+  -- 04. AVG – Average Salary
+  -- Returns the average salary in the company
 SELECT 
     AVG(salary) AS average_salary
 FROM employees;
 
-  -- MIN – Lowest Salary
+  -- 05. MIN – Lowest Salary
   -- Finds the lowest salary in the company 
 SELECT 
     MIN(salary) AS minimum_salary
 FROM employees;
 
-  -- MAX – Highest Salary
+  -- 06. MAX – Highest Salary
   -- Finds the highest salary in the company
 SELECT 
     MAX(salary) AS maximum_salary
 FROM employees;
 
-  -- AVG + GROUP BY – Department Average Salary
+  -- 07. AVG + GROUP BY – Department Average Salary
   -- Shows average salary per department
 SELECT 
     dept_id,
@@ -55,7 +55,7 @@ SELECT
 FROM employees
 GROUP BY dept_id;
 
-  -- HAVING + COUNT – Departments with 2+ Employees
+  -- 08. HAVING + COUNT – Departments with 2+ Employees
   -- Filters departments that have at least 2 employees 
 SELECT 
     dept_id,
@@ -64,41 +64,41 @@ FROM employees
 GROUP BY dept_id
 HAVING COUNT(*) >= 2;
 
-  -- CONCAT – Full Name Creation
+  -- 09. CONCAT – Full Name Creation
   -- Combines first and last name into a full name
 SELECT 
     CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees;
 
-  -- LENGTH – Name Length
-  -- Returns number of characters in first name. 
+  -- 10. LENGTH – Name Length
+  -- Returns number of characters in first name
 SELECT 
     first_name,
     LENGTH(first_name) AS name_length
 FROM employees;
 
-  -- UPPER – Uppercase Conversion
+  -- 11.UPPER – Uppercase Conversion
   -- Converts first names to uppercase
 SELECT 
     UPPER(first_name) AS uppercase_name
 FROM employees;
 
-  -- LOWER – Lowercase Conversion
+  -- 12. LOWER – Lowercase Conversion
   -- Converts email addresses to lowercase
 SELECT 
     LOWER(email) AS lowercase_email
 FROM employees;
 
-  -- TRIM – Remove Extra Spaces
+  -- 13. TRIM – Remove Extra Spaces
   -- Removes spaces from beginning and end of a string
 SELECT 
     TRIM('   HR System   ') AS trimmed_text;
 
-  -- NOW – Current Date & Time
+  -- 14. NOW – Current Date & Time
   -- Returns current system timestamp
 SELECT NOW() AS current_datetime;
 
-  -- DATE_FORMAT – Format Hire Date
+  -- 15. DATE_FORMAT – Format Hire Date
   -- Formats hire date into readable format (DD-MM-YYYY)
 SELECT 
     first_name,
@@ -106,7 +106,7 @@ SELECT
     DATE_FORMAT(hire_date, '%d-%m-%Y') AS formatted_hire_date
 FROM employees;
 
-  -- DATEDIFF – Days in Company
+  -- 16. DATEDIFF – Days in Company
   -- Calculates how many days an employee has worked in the company
 SELECT 
     first_name,
@@ -114,22 +114,22 @@ SELECT
     DATEDIFF(CURDATE(), hire_date) AS days_in_company
 FROM employees;
 
-  -- IFNULL – Replace NULL Values
-  -- Retrieves the first_name column from the employees table.
+  -- 17. IFNULL – Replace NULL Values
+  -- Retrieves the first_name column from the employees table
 SELECT 
     first_name,
     IFNULL(phone, 'No Phone Number') AS contact_number
 FROM employees;
 
-  -- NULL HANDLING – IFNULL (DATA CLEANING QUERY)
+  -- 18. NULL HANDLING – IFNULL (DATA CLEANING QUERY)
   -- Displays employee names and ensures that every employee
-  -- has a readable contact value, even if the phone number is missing.
+  -- has a readable contact value, even if the phone number is missing
 SELECT 
     first_name,
     IFNULL(phone, 'No Phone Number') AS contact_number
 FROM employees;
 
-  -- FILTERED GROUP ANALYSIS – HIGH SALARY DEPARTMENTS
+  -- 19. FILTERED GROUP ANALYSIS – HIGH SALARY DEPARTMENTS
   -- This query provides a full HR financial overview per department, showing:
   -- team size
   -- salary distribution
@@ -146,7 +146,7 @@ FROM employees
 GROUP BY dept_id;
 
 
--- FILTERED DEPARTMENT ANALYSIS (GROUP BY + HAVING + ORDER BY)
+-- 20. FILTERED DEPARTMENT ANALYSIS (GROUP BY + HAVING + ORDER BY)
 -- Groups employees by department
 -- Calculates employee count + average salary
 -- Filters only high-paying departments
