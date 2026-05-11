c7fdcb49b7684f14de6d0c532a09c7862d6fc585
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-11 14:50:29 +0300

    Update 01_03_Create_DB.sql

--- 01_03_Create_DB.sql
+++ 01_03_Create_DB.sql
@@ -1,10 +1,19 @@
-CREATE mydb;
+-- Database Operations: CREATE\DROP DATABASE, USE, SHOW
 
-SHOW DATABASES;
+
+CREATE mydb;
 
 DROP DATABASE mydb;
 
 CREATE DATABASE hr_system;
+
+USE hr_system;
+
+SHOW DATABASES;
+
+
+
+
 
 USE hr_system;
 
