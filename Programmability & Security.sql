d040e112c01c853b6ba77e95fc66461cdf964b1f
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-12 11:16:47 +0300

    Update Programmability & Security.sql

--- Programmability & Security.sql
+++ Programmability & Security.sql
@@ -5,3 +5,14 @@
 -- Export/ import mysqldump, sql files
 
 -- --THE END--
+
+CREATE USER analyst_user IDENTIFIED BY 'password123';
+
+GRANT SELECT ON employees TO analyst_user;
+GRANT SELECT ON departments TO analyst_user;
+
+CREATE ROLE hr_role;
+
+GRANT SELECT, INSERT, UPDATE ON employees TO hr_role;
+
+GRANT hr_role TO analyst_user;
