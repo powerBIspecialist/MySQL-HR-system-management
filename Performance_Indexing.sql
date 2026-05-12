90572b01c1825a0e6960fd618946173026524d80
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-12 11:12:43 +0300

    Delete Performance_Indexing.sql

--- Performance_Indexing.sql
+++
@@ -1,7 +0,0 @@
--- Create INDEX
--- Explain
--- Indexes on WHERE and JOIN columns
--- Composite indexes (multi-column)
--- Avoiding full table scans, query optimization
-
--- -- PRoject: Explain analysis + relevant indexes
