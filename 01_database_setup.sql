023e7b933e0a909e02e223b2d1024b59cb5f3110
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-12 08:46:03 +0300

    Update 01_database_setup.sql

--- 01_database_setup.sql
+++ 01_database_setup.sql
@@ -1,1 +1,20 @@
+    -- The database setup file initializes the environment by creating and selecting the database.
+    -- Database Operations: CREATE\DROP DATABASE, USE, SHOW
 
+-- Create a database named mydb
+CREATE DATABASE mydb;
+
+-- Delete the database named mydb
+DROP DATABASE mydb;
+
+-- Delete database if exists
+DROP DATABASE IF EXISTS hr_system;
+
+-- Create a new database named hr_system
+CREATE DATABASE hr_system;
+
+-- Select hr_system as the active database
+USE hr_system;
+
+-- Show all databases available on the server
+SHOW DATABASES;
