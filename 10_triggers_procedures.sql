8dbda7897c4bd3afcc598b80683e82e22a8917e7
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-12 09:10:07 +0300

    Update 10_triggers_procedures.sql

--- 10_triggers_procedures.sql
+++ 10_triggers_procedures.sql
@@ -1,1 +1,22 @@
 
+DELIMITER $$
+
+CREATE TRIGGER trg_salary_audit
+BEFORE UPDATE ON employees
+FOR EACH ROW
+BEGIN
+    IF NOT (OLD.salary <=> NEW.salary) THEN
+        INSERT INTO salary_audit (
+            emp_id,
+            old_salary,
+            new_salary
+        )
+        VALUES (
+            OLD.emp_id,
+            OLD.salary,
+            NEW.salary
+        );
+    END IF;
+END$$
+
+DELIMITER ;
